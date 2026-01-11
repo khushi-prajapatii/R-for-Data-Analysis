d899f755a3da89b8d9d827748e30f22677c0757c
Khushi Prajapati <khushiiprajapati1507@gmail.com>
2026-01-11 14:52:29 +0530

    Add files via upload

---
+++ day1_reading_dataset.R
@@ -0,0 +1,14 @@
+# Day-1: Reading and Exploring a Dataset in R
+
+# using a Built-in function mtcars
+
+#step1: Load the dataset
+data(mtcars)
+#s2: view data
+head(mtcars)
+#s3: Understand structure of data
+str(mtcars)
+
+#Findings:
+# - this a numeric data in tabular form
+# - it has 32 rows and 11 columns
